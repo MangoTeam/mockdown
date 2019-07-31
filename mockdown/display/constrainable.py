2d3a3af084f1f14a4251d57ade148ad2568174d7
Dylan Lukes <lukes.dylan@gmail.com>
2019-07-31 15:01:57 -0700

    fix alignment display for offset elements

--- mockdown/display/constrainable.py
+++ mockdown/display/constrainable.py
@@ -20,7 +20,6 @@ def alignment_constrainable_pair_style(pair: ConstrainablePair, scale=1):
     e2 = pair.anchor2.edge
 
     assert e1.attribute == e2.attribute
-    assert e1.position == e2.position
 
     position = e1.position
     union = (min(e1.interval[0], e2.interval[0]),
@@ -32,12 +31,24 @@ def alignment_constrainable_pair_style(pair: ConstrainablePair, scale=1):
         "border: 0.5px dashed blue;"
     ]
 
-    style_args = (union[0], union[1], position)
+    style_args = [union[0], union[1]]
     style_kwargs = {'scale': scale}
 
     if e1.attribute in ['left', 'right']:
+        if e1.attribute == 'left':
+            position = min(e1.position, e2.position)
+            style_args.append(position)
+        if e1.attribute == 'right':
+            position = max(e1.position, e2.position)
+            style_args.append(position)
         style.append(vertical_line_style(*style_args, **style_kwargs))
     elif e1.attribute in ['top', 'bottom']:
+        if e1.attribute == 'top':
+            position = min(e1.position, e2.position)
+            style_args.append(position)
+        if e1.attribute == 'bottom':
+            position = max(e1.position, e2.position)
+            style_args.append(position)
         style.append(horizontal_line_style(*style_args, **style_kwargs))
     else:
         raise NotImplementedError()
