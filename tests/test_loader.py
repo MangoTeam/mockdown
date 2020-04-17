7e7a921822834f4b1bc3c111df3b78bd693e1824
Dylan Lukes <lukes.dylan@gmail.com>
2020-04-16 22:27:55 -0700

    some loader testing

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,4 +1,8 @@
-from mockdown.model import ZViewLoader
+from fractions import Fraction
+
+import pytest  # type: ignore
+
+from mockdown.model import QViewLoader, ZViewLoader
 
 
 class TestZViewLoader:
@@ -8,4 +12,30 @@ class TestZViewLoader:
             'name': 'root',
             'rect': [0, 0, 100, 100]
         })
-        print(view)
+
+        view = loader.load_dict({
+            'name': 'root',
+            'rect': [0.0, 0.0, 100.0, 100.0]
+        })
+
+        with pytest.raises(Exception):
+            view = loader.load_dict({
+                'name': 'root',
+                'rect': [0.0, 0.0, 100.5, 100.5]
+            })
+
+
+class TestQViewLoader:
+    def test_string_fractions(self) -> None:
+        loader = QViewLoader()
+        view = loader.load_dict({
+            'name': 'root',
+            'rect': [0, 0, '1/2', '1/2']
+        })
+
+        view = loader.load_dict({
+            'name': 'root',
+            'rect': [0.0, 0.0, 0.5, 0.5]
+        })
+        assert view.right == Fraction(1, 2)
+        assert view.bottom == Fraction(1, 2)
