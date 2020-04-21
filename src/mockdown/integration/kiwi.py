13c71fad9381cc52425f4d615db9a44654102984
Dylan Lukes <lukes.dylan@gmail.com>
2020-04-20 19:37:00 -0700

    tentative feature parity with before?

--- src/mockdown/integration/kiwi.py
+++ src/mockdown/integration/kiwi.py
@@ -3,11 +3,12 @@ import kiwisolver  # type: ignore
 from ..constraint import IConstraint
 
 
+# todo: I had to add float(...) here because kiwisolver doesn't understand fractions. – Dylan
 def constraint_to_kiwi(constraint: IConstraint, strength: str = 'required') -> kiwisolver.Constraint:
     yv = kiwisolver.Variable(str(constraint.y_id))
     if constraint.x_id:
         xv = kiwisolver.Variable(str(constraint.x_id))
-        return constraint.op(yv, xv * constraint.a) | strength
+        return constraint.op(yv, xv * (float(constraint.a))) | strength
     else:
         # print("me:", self)
-        return constraint.op(yv, constraint.a) | strength
+        return constraint.op(yv, float(constraint.a)) | strength
