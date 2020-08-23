10958b4a82eb925568cee4a743c6570d1e3fb16a
Dylan Lukes <dlukes@eng.ucsd.edu>
2020-08-22 19:19:35 -0700

    do not import prolog on windows

--- src/mockdown/instantiation/__init__.py
+++ src/mockdown/instantiation/__init__.py
@@ -1,10 +1,18 @@
+import os
+
 from .numpy import NumpyConstraintInstantiator
 from .types import IConstraintInstantiator
-from .prolog import PrologConstraintInstantiator
 
-__all__ = [
-    'IConstraintInstantiator',
-    'PrologConstraintInstantiator',
-    'NumpyConstraintInstantiator'
-]
+if os.name != 'nt':
+    from .prolog import PrologConstraintInstantiator
+    __all__ = [
+        'IConstraintInstantiator',
+        'PrologConstraintInstantiator',
+        'NumpyConstraintInstantiator'
+    ]
+else:
+    __all__ = [
+        'IConstraintInstantiator',
+        'NumpyConstraintInstantiator'
+    ]
 
